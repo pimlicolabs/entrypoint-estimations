894b6ccc4ea1e658cf4132378f778d1d194e183b
mouseless <97399882+mouseless-eth@users.noreply.github.com>
2024-10-11 17:50:11 +0100

    add v06 code override contract

--- script/PimlicoEntryPointSimulations.s.sol
+++ script/PimlicoEntryPointSimulations.s.sol
@@ -2,23 +2,18 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
-import "../src/PimlicoEntryPointSimulations.sol";
+import "../src/v07/PimlicoEntryPointSimulations.sol";
 
 contract PimlicoEntryPointSimulationsScript is Script {
     function setUp() public {}
 
-    function run()
-        public
-        returns (address pimlicoEntryPointSimulationsAddress)
-    {
+    function run() public returns (address pimlicoEntryPointSimulationsAddress) {
         address deployerSigner = vm.envAddress("SIGNER");
         bytes32 salt = vm.envBytes32("SALT");
 
         vm.startBroadcast(deployerSigner);
 
-        pimlicoEntryPointSimulationsAddress = address(
-            new PimlicoEntryPointSimulations{salt: salt}()
-        );
+        pimlicoEntryPointSimulationsAddress = address(new PimlicoEntryPointSimulations{salt: salt}());
 
         vm.stopBroadcast();
     }
